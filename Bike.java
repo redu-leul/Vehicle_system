b68285039b14f416a659aff8b5878679f3667042
redu-leul <leulrediet@gmail.com>
2026-05-17 15:31:42 +0300

    new bike class

---
+++ Bike.java
@@ -0,0 +1,28 @@
+public class Bike extends Vehicle {
+    private boolean hascarrier;
+    private String size;
+
+    public Bike(String Brand, int Year, int Speed, boolean hascarrier, String size){
+        super(Brand, Year, Speed);
+        this.hascarrier = hascarrier;
+        this.size = size;
+    }
+    public boolean gethascarrier(){
+        return hascarrier;
+    }
+    public String getsize(){
+        return size;
+    }
+    @Override
+    public void honk(){
+        IO.println("Bike Honks: Ring Ring!");
+    }
+    @Override
+    public void display(){
+        super.display();
+        IO.println("HAS CARRIER:" + hascarrier);
+        IO.println("SIZE:" + size);
+    }
+
+    
+}
